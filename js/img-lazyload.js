4a58676690bb6a82b7b90ee5ec84faa7d6b0a603
4rozeN <mail4chen@foxmail.com>
2025-06-29 11:21:05 +0800

    Site updated: 2025-06-29 11:21:03

--- js/img-lazyload.js
+++
@@ -1,1 +0,0 @@
-(()=>{for(let e of document.querySelectorAll("img[lazyload]"))Fluid.utils.waitElementVisible(e,function(){e.removeAttribute("srcset"),e.removeAttribute("lazyload")},CONFIG.lazyload.offset_factor)})(window);
